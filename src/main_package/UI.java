74b91652981e760b0d0065bdec2d5fa9126301c9
Talip Vural <talip.vu@gmail.com>
2016-01-13 22:29:57 +0100

    Add: remis button in menu, chess-gamepanel, make frame not resizable

--- src/main_package/UI.java
+++ src/main_package/UI.java
@@ -1,9 +1,13 @@
 package main_package;
 
+import java.awt.Color;
+import java.awt.GridLayout;
+
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JPanel;
 
 class UI {
 
@@ -15,7 +19,8 @@ class UI {
 	JFrame mainFrame;
 	JMenuBar menuBar;
 	JMenu menu1;
-	JMenuItem mItem1;
+	JMenuItem mItem1, mItem2;
+	JPanel gamePanel;
 	
 	public static void main(String[] args) {	
 		UI ui = new UI();
@@ -28,16 +33,25 @@ class UI {
 	void buildGUI(){
 		
 		mainFrame = new JFrame("Chess");
-		mainFrame.setBounds(400, 200, 800, 800);
+		mainFrame.setBounds(400, 200, 1200, 800);
 		
 		menuBar = new JMenuBar();
 		menu1 = new JMenu("Game");
 		mItem1 = new JMenuItem("New");
+		mItem2 = new JMenuItem("Remis");
+		gamePanel = new JPanel();
+		gamePanel.setBounds(50, 25, 700, 700);
+		gamePanel.setLayout(null);
+		gamePanel.setBackground(Color.RED);
+		gamePanel.setLayout(new GridLayout());
 		
 		mainFrame.setJMenuBar(menuBar);
+		mainFrame.add(gamePanel);
 		menuBar.add(menu1);
 		menu1.add(mItem1);
+		menu1.add(mItem2);
 		
+		mainFrame.setResizable(false);
 		mainFrame.setLayout(null);
 		mainFrame.setVisible(true);;
 	}
