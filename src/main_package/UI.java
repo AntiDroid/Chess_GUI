f5f36f9e8240dbca07a18d310ab199c09b0ff516
AntiDroid <talip.vu@gmail.com>
2016-01-13 14:56:12 +0100

    Add: JFrame and JMenuBar with one JMenuBarItem

--- src/main_package/UI.java
+++ src/main_package/UI.java
@@ -1,5 +1,10 @@
 package main_package;
 
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+
 class UI {
 
 	Game g1;
@@ -7,16 +12,34 @@ class UI {
 	Boolean whiteTurn = true;
 	int player;
 	
+	JFrame mainFrame;
+	JMenuBar menuBar;
+	JMenu menu1;
+	JMenuItem mItem1;
+	
 	public static void main(String[] args) {	
 		UI ui = new UI();
-		ui.buildUI();
+		ui.buildGUI();
 	}
 
 	/**
 	 * Spielverwaltung mit Benutzerinteraktionen
 	 */
-	void buildUI(){
+	void buildGUI(){
 		
+		mainFrame = new JFrame("Chess");
+		mainFrame.setBounds(400, 200, 800, 800);
+		
+		menuBar = new JMenuBar();
+		menu1 = new JMenu("Game");
+		mItem1 = new JMenuItem("New");
+		
+		mainFrame.setJMenuBar(menuBar);
+		menuBar.add(menu1);
+		menu1.add(mItem1);
+		
+		mainFrame.setLayout(null);
+		mainFrame.setVisible(true);;
 	}
 
 	}
